13216fa9aeae212c4a44ab8aba8b14fe69b09fb6
seanjulagay <seanaustinj@gmail.com>
2022-10-31 16:11:55 +0800

    Zone lock, reflect level load aligned to zone
    - Locked zones display locked icon, is not clickable
    - Level selection and star display correspond correctly with selected zone

--- Assets/Scripts/MainMenu/LevelSelectManagerScript.cs
+++ Assets/Scripts/MainMenu/LevelSelectManagerScript.cs
@@ -8,6 +8,7 @@ public class LevelSelectManagerScript : MonoBehaviour
     public List<GameObject> zoneIconButton;
     public List<Sprite> zoneIconSprite;
     public List<Sprite> zoneIconSpriteSel;
+    public List<Sprite> zoneIconSpriteLocked;
 
     public List<GameObject> levelIconButton;
     public List<Sprite> starIconSprite; // 0 = 0 stars, 1 = 1 star, etc.
@@ -17,13 +18,14 @@ public class LevelSelectManagerScript : MonoBehaviour
 
     int selectedZone; // 0 = euphotic, 1 = dysphotic, 2 = aphotic
     int selectedLevel;
+    int zoneMaxLevel;
 
     // =========== ZONE SELECT =========== //
 
     public void InitializeZoneButtons()
     {
         selectedZone = 0;
-        ClearZoneIcons();
+        UpdateZoneIcons();
         zoneIconButton[0].GetComponent<Image>().sprite = zoneIconSpriteSel[0];
     }
 
@@ -31,16 +33,24 @@ public class LevelSelectManagerScript : MonoBehaviour
     {
         selectedZone = index;
         Debug.Log("Pressed button: " + selectedZone);
-        ClearZoneIcons();
+        UpdateZoneIcons();
 
         zoneIconButton[index].GetComponent<Image>().sprite = zoneIconSpriteSel[index];
     }
 
-    public void ClearZoneIcons()
+    public void UpdateZoneIcons()
     {
-        for (int i = 0; i < zoneIconButton.Count; i++)
+        for (int i = 0; i <= ProfileManagerScript.activeUser.currentZone; i++)
         {
+            int local_i = i;
+            zoneIconButton[i].GetComponent<Button>().onClick.RemoveAllListeners();
             zoneIconButton[i].GetComponent<Image>().sprite = zoneIconSprite[i];
+            zoneIconButton[i].GetComponent<Button>().onClick.AddListener(delegate { UpdateZonePanel(local_i); });
+        }
+
+        for (int i = ProfileManagerScript.activeUser.currentZone + 1; i < 3; i++)
+        {
+            zoneIconButton[i].GetComponent<Image>().sprite = zoneIconSpriteLocked[i];
         }
     }
 
@@ -60,20 +70,29 @@ public class LevelSelectManagerScript : MonoBehaviour
             levelIconButton[i].transform.Find("LevelStars").GetComponent<Image>().sprite = starIconSprite[0];
         }
 
-        if (ProfileManagerScript.activeUser.currentZone == selectedZone)
-        { // load proper levels
-
-        }
-        
-        for (int i = 0; i < ProfileManagerScript.activeUser.currentLevel; i++)
+        // Load proper levels
+        if (selectedZone == ProfileManagerScript.activeUser.currentZone) // if user selects current zone, load until locked levels
         {
-            levelIconButton[i].GetComponent<Image>().sprite = levelIconSprite[i];
-            levelIconButton[i].transform.Find("LevelStars").GetComponent<Image>().sprite = starIconSprite[ProfileManagerScript.activeUser.levelStars[(5 * selectedZone) + i]];
-        }
+            for (int i = 0; i <= ProfileManagerScript.activeUser.currentLevel; i++)
+            { // load working buttons until user's current level
+                Debug.Log("User selected current zone");
+                levelIconButton[i].GetComponent<Image>().sprite = levelIconSprite[i];
+                levelIconButton[i].transform.Find("LevelStars").GetComponent<Image>().sprite = starIconSprite[ProfileManagerScript.activeUser.levelStars[(5 * selectedZone) + i]];
+            }
 
-        for (int i = ProfileManagerScript.activeUser.currentLevel + 1; i < 5; i++)
-        { // load locked levels
-            levelIconButton[i].GetComponent<Image>().sprite = levelIconSprite[0];
+            for (int i = ProfileManagerScript.activeUser.currentLevel + 1; i < 5; i++)
+            { // load locked levels
+                levelIconButton[i].GetComponent<Image>().sprite = levelIconSprite[5];
+            }
+        }
+        else if (selectedZone < ProfileManagerScript.activeUser.currentZone)
+        { // if user selects previous zone, load everything
+            for (int i = 0; i < 5; i++)
+            {
+                Debug.Log("User selected finished zone");
+                levelIconButton[i].GetComponent<Image>().sprite = levelIconSprite[i];
+                levelIconButton[i].transform.Find("LevelStars").GetComponent<Image>().sprite = starIconSprite[ProfileManagerScript.activeUser.levelStars[(5 * selectedZone) + i]];
+            }
         }
     }
 }
