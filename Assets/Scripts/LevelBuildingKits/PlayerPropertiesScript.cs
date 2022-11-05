e71d6479921fa3b658d50f5682d28c3081794bf9
seanjulagay <seanaustinj@gmail.com>
2022-11-05 22:33:33 +0800

    Centralized props script, oxygen system

---
+++ Assets/Scripts/LevelBuildingKits/PlayerPropertiesScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPropertiesScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float oxygenCount = 99f;
+    public float playerScore;
+    public bool underwater = false;
+
+    float oxygenReplenishRate = 5f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PlayerOxygenHandler();
+        PlayerPhysicsHandler();
+    }
+
+    void PlayerOxygenHandler()
+    {
+        if (underwater == true && oxygenCount > 0f)
+        {
+            oxygenCount -= Time.deltaTime;
+        }
+        else if (underwater == false && oxygenCount < 100f)
+        {
+            oxygenCount += Time.deltaTime * oxygenReplenishRate;
+        }
+    }
+
+    void PlayerPhysicsHandler()
+    {
+        if (underwater == true)
+        {
+            rb.gravityScale = UniversalValues.underwaterGravity;
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("WaterBlock"))
+        {
+            underwater = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("WaterBlock"))
+        {
+            underwater = false;
+        }
+    }
+}
