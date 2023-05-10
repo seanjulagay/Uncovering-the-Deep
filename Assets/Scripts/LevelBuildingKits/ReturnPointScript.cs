803b4d9e238ac53fd6ae4002da3109cca26f6f6e
seanniekins <seanaustinj@gmail.com>
2023-05-10 21:40:49 +0800

    Star system timer hotfix

--- Assets/Scripts/LevelBuildingKits/ReturnPointScript.cs
+++ Assets/Scripts/LevelBuildingKits/ReturnPointScript.cs
@@ -15,6 +15,8 @@ public class ReturnPointScript : MonoBehaviour
     TMP_Text levelCompleteHeaderText;
     Sprite[] levelCompleteStarsArr;
 
+    bool displayDetails = false;
+
     void Start()
     {
         uiManagerScript = GameObject.Find("UIManager").GetComponent<UIManagerScript>();
@@ -27,6 +29,14 @@ public class ReturnPointScript : MonoBehaviour
         levelCompleteStarsArr = new Sprite[4] { uiManagerScript.levelCompleteStars0, uiManagerScript.levelCompleteStars1, uiManagerScript.levelCompleteStars2, uiManagerScript.levelCompleteStars3 };
     }
 
+    void Update()
+    {
+        if (displayDetails == true)
+        {
+            Debug.Log("gameManagerScript.timeSpentSecs: " + gameManagerScript.timeSpentSecs + " gameManagerScript.starsThisLevel: " + gameManagerScript.starsThisLevel);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player" && gameManagerScript.activateReturnPoint == true)
@@ -37,26 +47,48 @@ public class ReturnPointScript : MonoBehaviour
             }
             else if (gameManagerScript.endGameType == 1) // timer type
             {
-                if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[0])
+                if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[3])
                 {
-                    gameManagerScript.starsThisLevel = 0;
-                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[0];
-                }
-                else if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[1])
-                {
-                    gameManagerScript.starsThisLevel = 1;
-                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[1];
+                    gameManagerScript.starsThisLevel = 3;
+                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[3];
                 }
                 else if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[2])
                 {
                     gameManagerScript.starsThisLevel = 2;
                     levelCompleteStarsImg.sprite = levelCompleteStarsArr[2];
                 }
+                else if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[1])
+                {
+                    gameManagerScript.starsThisLevel = 1;
+                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[1];
+                }
                 else
                 {
-                    gameManagerScript.starsThisLevel = 3;
-                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[3];
+                    gameManagerScript.starsThisLevel = 0;
+                    levelCompleteStarsImg.sprite = levelCompleteStarsArr[0];
                 }
+
+                displayDetails = true;
+                // if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[0])
+                // {
+                //     gameManagerScript.starsThisLevel = 0;
+                //     levelCompleteStarsImg.sprite = levelCompleteStarsArr[0];
+                // }
+                // else if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[1])
+                // {
+                //     gameManagerScript.starsThisLevel = 1;
+                //     levelCompleteStarsImg.sprite = levelCompleteStarsArr[1];
+                // }
+                // else if (gameManagerScript.timeSpentSecs <= gameManagerScript.timeGoals[2])
+                // {
+                //     gameManagerScript.starsThisLevel = 2;
+                //     levelCompleteStarsImg.sprite = levelCompleteStarsArr[2];
+                // }
+                // else
+                // {
+                //     gameManagerScript.starsThisLevel = 3;
+                //     levelCompleteStarsImg.sprite = levelCompleteStarsArr[3];
+                // }
             }
             levelCompleteHeaderText.text = "Level Complete!";
             levelCompletePanel.SetActive(true);
