98f89173d294097e2c726ce60d7b5ec2488cb503
Julia Avesta <juliaavesta@gmail.com>
2023-05-24 14:25:44 +0800

    BackButton Linked to Main from Aquarium + Bubbles and Audio to the Aquarium again

--- Assets/Scripts/Aquarium/AquariumButtonManagerScript.cs
+++ Assets/Scripts/Aquarium/AquariumButtonManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AquariumButtonManagerScript : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class AquariumButtonManagerScript : MonoBehaviour
     Button achievementsCloseButton;
     GameObject almanacPanel;
     GameObject achievementsPanel;
+    Button backToMainMenuButton;
 
     void Start()
     {
@@ -19,6 +21,9 @@ public class AquariumButtonManagerScript : MonoBehaviour
         achievementsCloseButton = GameObject.Find("AchievementsCloseButton").GetComponent<Button>();
         achievementsCloseButton.onClick.AddListener(CloseAchievements);
         achievementsPanel = GameObject.Find("AchievementsPanel");
+
+        backToMainMenuButton = GameObject.Find("buttonBackAquarium").GetComponent<Button>();
+        backToMainMenuButton.onClick.AddListener(backToMainMenu);
     }
 
     public void CloseAlmanac()
@@ -29,5 +34,9 @@ public class AquariumButtonManagerScript : MonoBehaviour
     public void CloseAchievements()
     {
         achievementsPanel.SetActive(false);
+    }
+
+    public void backToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
     }
 }
