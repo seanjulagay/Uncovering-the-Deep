803c9ca4f3925b6aec6576a0a33ecb7b564430f7
seanniekins <seanaustinj@gmail.com>
2023-09-13 22:34:47 +0800

    Added manual interaction feature for animals

--- Assets/Scripts/LevelBuildingKits/DialogueTriggerScript.cs
+++ Assets/Scripts/LevelBuildingKits/DialogueTriggerScript.cs
@@ -7,6 +7,8 @@ public class DialogueTriggerScript : MonoBehaviour
 {
     public int creatureID = -1;
 
+    public bool isQuizAnimal = false;
+    public bool autoDisplay = true;
     public bool isPartOfAnimalsMetCount = false;
     public bool onlyShowOnce = false;
     public bool automaticRepeat = false;
@@ -28,6 +30,7 @@ public class DialogueTriggerScript : MonoBehaviour
     bool firstOpen = true;
     bool listenForRepeat = false;
     bool listenForNext = false;
+    bool displayingDialogue = false;
 
     ObjectivesManagerScript objectivesManagerScript;
 
@@ -43,10 +46,28 @@ public class DialogueTriggerScript : MonoBehaviour
         dialoguePanelManagerScript = GameObject.Find("DialoguePanelManager").GetComponent<DialoguePanelManagerScript>();
         objectivesManagerScript = GameObject.Find("ObjectivesManager").GetComponent<ObjectivesManagerScript>();
 
+        try
+        {
+            helperTextObj = GameObject.Find("HelperText");
+            helperText = helperTextObj.GetComponent<TMP_Text>();
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("This scene has no helper text objects. " + e);
+        }
         // helperTextObj = GameObject.Find("HelperText");
         // helperText = helperTextObj.GetComponent<TMP_Text>();
 
         InitializeEmpty();
+        DisableColliderAutoDisplay();
+    }
+
+    void DisableColliderAutoDisplay()
+    {
+        if (gameObject.GetComponent<BoxCollider2D>().isTrigger == false)
+        {
+            autoDisplay = false;
+        }
     }
 
     void InitializeEmpty()
@@ -95,32 +116,9 @@ public class DialogueTriggerScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "PlayerTrigger")
+        if (autoDisplay == true && other.gameObject.name == "PlayerTrigger")
         {
-            SetDatabaseUnlocked();
-            if (gameObject.name == "QuizAnimal")
-            {
-                if (objectivesManagerScript.animalsMetCurrent < objectivesManagerScript.animalsMetMax)
-                {
-                    StartDialogue();
-
-                }
-            }
-            else
-            {
-                if (firstOpen == true)
-                {
-                    StartDialogue();
-                    if (isPartOfAnimalsMetCount)
-                    {
-                        gameManagerScript.animalsMet++;
-                    }
-                }
-                else if (firstOpen == false && automaticRepeat == true)
-                {
-                    StartDialogue();
-                }
-            }
+            StartDialogue();
         }
     }
 
@@ -158,44 +156,87 @@ public class DialogueTriggerScript : MonoBehaviour
     {
         if (other.gameObject.name == "PlayerTrigger")
         {
-            // if (firstOpen == true)
-            // {
-            //     if (isPartOfAnimalsMetCount)
-            //     {
-            //         gameManagerScript.animalsMet++;
-            //     }
-            //     StartDialogue();
-            // }
-            // else
-            // {
-            //     Debug.Log("HELLOOOOOOOO");
-            //     helperText.text = "Press spacebar to interact";
-            //     listenForRepeat = true;
-            // }
-
-            if (firstOpen == false && automaticRepeat == false)
+            if (isQuizAnimal == false)
             {
-                // helperText.text = "Press spacebar to repeat dialogue";
-                listenForRepeat = true;
+                if (firstOpen == false && automaticRepeat == false)
+                {
+                    if (displayingDialogue == true)
+                    {
+                        listenForRepeat = false;
+                    }
+                    else
+                    {
+                        helperText.text = "Press spacebar to repeat dialogue";
+                        listenForRepeat = true;
+                    }
+                }
+                else if (firstOpen == true)
+                {
+                    if (displayingDialogue == true)
+                    {
+                        listenForRepeat = false;
+                    }
+                    else
+                    {
+                        if (autoDisplay == false)
+                        {
+                            helperText.text = "Press spacebar to interact";
+                            listenForRepeat = true;
+                        }
+                    }
+                }
             }
         }
+
+        // if (other.gameObject.name == "PlayerTrigger")
+        // {
+        //     // if (firstOpen == true)
+        //     // {
+        //     //     if (isPartOfAnimalsMetCount)
+        //     //     {
+        //     //         gameManagerScript.animalsMet++;
+        //     //     }
+        //     //     StartDialogue();
+        //     // }
+        //     // else
+        //     // {
+        //     //     Debug.Log("HELLOOOOOOOO");
+        //     //     helperText.text = "Press spacebar to interact";
+        //     //     listenForRepeat = true;
+        //     // }
+
+        //     // if (firstOpen == false && automaticRepeat == false)
+        //     // {
+        //     //     helperText.text = "Press spacebar to repeat dialogue";
+        //     //     listenForRepeat = true;
+        //     // }
+        //     // else if (firstOpen == true)
+        //     // {
+        //     //     helperText.text = "Press spacebar to interact";
+        //     //     listenForRepeat = true;
+        //     // }
+        // }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.name == "PlayerTrigger")
         {
-            if (firstOpen == false)
-            {
-                // helperText.text = "";
-                listenForRepeat = false;
-            }
+            listenForNext = false;
+            listenForRepeat = false;
+            helperText.text = "";
+            // if (firstOpen == false)
+            // {
+            //     // helperText.text = "";
+            //     listenForRepeat = false;
+            // }
         }
     }
 
     void StartDialogue()
     {
-        // helperText.text = "";
+        helperText.text = "";
+        displayingDialogue = true;
         index = 0;
         listenForNext = true;
         listenForRepeat = false;
@@ -211,9 +252,11 @@ public class DialogueTriggerScript : MonoBehaviour
 
     void EndDialogue()
     {
+        displayingDialogue = false;
         firstOpen = false;
         index = 0;
         listenForNext = false;
+        listenForRepeat = true;
         dialoguePanelManagerScript.HideDialogue();
         if (onlyShowOnce == true)
         {
@@ -240,7 +283,31 @@ public class DialogueTriggerScript : MonoBehaviour
             }
             else if (listenForRepeat == true)
             {
-                StartDialogue();
+                Debug.Log("INTERACTBUTTONLISTENER: Listening for repeat");
+                SetDatabaseUnlocked();
+                if (gameObject.name == "QuizAnimal")
+                {
+                    if (objectivesManagerScript.animalsMetCurrent < objectivesManagerScript.animalsMetMax)
+                    {
+                        StartDialogue();
+                    }
+                }
+                else
+                {
+                    if (firstOpen == true)
+                    {
+                        StartDialogue();
+                        if (isPartOfAnimalsMetCount)
+                        {
+                            gameManagerScript.animalsMet++;
+                        }
+                    }
+                    else
+                    {
+                        StartDialogue();
+                    }
+                }
+                // StartDialogue();
             }
         }
     }
